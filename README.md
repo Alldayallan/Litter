2342a76d3f6859ce4be2429051d9021514a89057
Allan <mail@allanreid.co>
2021-04-11 11:37:34 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Litter
+
+# litter (Litter)
+
+# Node.js - Socket.iO - MondgoDB
+
+## Install the dependencies
+```
+bash, Z shell or the like
+run
+npm install
+```
+
+## Run the server
+```
+npm start
+```
---
+++ database.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
+
+class Database {
+
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        mongoose.connect("mongodb+srv://admin:R091568j!969@littercluster.f4k4y.mongodb.net/LitterDB?retryWrites=true&w=majority")
+        .then(() => {
+            console.log("database connection successful");
+        })
+        .catch((err) => {
+            console.log("database connection error " + err);
+        })
+    }
+}
+
+module.exports = new Database();
