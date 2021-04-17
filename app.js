d9dd60eb9627536d1f42ca81e0fa6efffc956b7e
Allan <mail@allanreid.co>
2021-04-17 10:15:16 -0700

    profile pic added to database and profile pic complete

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
 const postRoute = require('./routes/postRoutes');
 const profileRoute = require('./routes/profileRoutes');
+const uploadRoute = require('./routes/uploadRoutes');
 const logoutRoute = require('./routes/logout');
 
 // Api routes
@@ -39,6 +40,7 @@ app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/posts", middleware.requireLogin, postRoute);
 app.use("/profile", middleware.requireLogin, profileRoute);
+app.use("/uploads", uploadRoute);
 app.use("/logout", logoutRoute);
 
 app.use("/api/posts", postsApiRoute);
