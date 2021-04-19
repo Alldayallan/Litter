c24f7b37b3fe233eadd9d746736f74ee797f11a5
Allan <mail@allanreid.co>
2021-04-18 17:12:58 -0700

    group chat started

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ const postRoute = require('./routes/postRoutes');
 const profileRoute = require('./routes/profileRoutes');
 const uploadRoute = require('./routes/uploadRoutes');
 const searchRoute = require('./routes/searchRoutes');
+const messagesRoute = require('./routes/messagesRoutes');
 const logoutRoute = require('./routes/logout');
 
 // Api routes
@@ -43,6 +44,7 @@ app.use("/posts", middleware.requireLogin, postRoute);
 app.use("/profile", middleware.requireLogin, profileRoute);
 app.use("/uploads", uploadRoute);
 app.use("/search", middleware.requireLogin, searchRoute);
+app.use("/messages", middleware.requireLogin, messagesRoute);
 app.use("/logout", logoutRoute);
 
 app.use("/api/posts", postsApiRoute);
