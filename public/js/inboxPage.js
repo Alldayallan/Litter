25934551faf5a70e4c5f8d685bd7694901262333
Allan <mail@allanreid.co>
2021-04-24 16:42:44 -0700

    chat list started

---
+++ public/js/inboxPage.js
@@ -0,0 +1,53 @@
+$(document).ready(() => {
+    $.get("/api/chats", (data, status, xhr) => {
+        if(xhr.status == 400) {
+            alert("Could not get chat list.");
+        }
+        else {
+            outputChatList(data, $(".resultsContainer"));
+        }
+    })
+})
+
+function outputChatList(chatList, container) {
+    chatList.forEach(chat => {
+        var html = createChatHtml(chat);
+        container.append(html);
+    })
+
+    if(chatList.length == 0) {
+        container.append("<span class='noResults'>Nothing to show.</span>");
+    }
+}
+
+function createChatHtml(chatData) {
+    var chatName = getChatName(chatData);
+    var image = ""; // TODO
+    var latestMessage = "This is the latest message";
+
+    return `<a href='/messages/${chatData._id}' class='resultListItem'>
+                <div class='resultsDetailsContainer'>
+                    <span class='heading'>${chatName}</span>
+                    <span class='subText'>${latestMessage}</span>
+                </div>
+            </a>`;
+}
+
+function getChatName(chatData) {
+    var chatName = chatData.chatName;
+
+    if(!chatName) {
+        var otherChatUsers = getOtherChatUsers(chatData.users);
+        var namesArray = otherChatUsers.map(user => user.firstName + " " + user.lastName);
+        chatName = namesArray.join(", ")
+    }
+
+    return chatName;
+}
+
+function getOtherChatUsers(users) {
+    if(users.length == 1) return users;
+
+    return users.filter(user => user._id != userLoggedIn._id);
+
+}
