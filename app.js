4ba3c901f8f6068b2f113ee128c319a8301f9b98
Allan Reid <allanreid@Allans-MacBook-Pro.local>
2021-06-12 12:48:45 -0700

    config for web

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 3003;
 const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require("body-parser");
--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,6 +1,14 @@
 var connected = false;
 
-var socket = io("http://localhost:3003")
+//var socket = io("http://localhost:3003")
+var socketUrl = "http://litter.media"; // ADD YOUR HOSTED URL HERE
+ 
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    // If in localhost, use this url
+    socketUrl = "https://localhost:3003";
+}
+ 
+var socket = io(socketUrl);
 socket.emit("setup", userLoggedIn);
 
 socket.on("connected", () => connected = true);
